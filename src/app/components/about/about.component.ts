67aa084e9e11479e9854d997d4a20c5b52afabf5
Lingareddy Annela <alreddy_mac@AL-2.local>
2019-01-19 23:09:10 -0500

    Git Check

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('Git test!');
+  }
 
   ngOnInit() {
   }
+
 
 }
